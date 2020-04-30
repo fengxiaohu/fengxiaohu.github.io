25373cbcdd0177f48fad5559659b0d386c0f2626
xiaohu <975326527@qq.com>
2020-04-30 21:11:31 +0800

    Site updated: 2020-04-30 21:11:31

--- 2020/04/07/Leetcode-155-Min-Stack/Min_stack.py
+++ 2020/04/07/Leetcode-155-Min-Stack/Min_stack.py
@@ -1,3 +1,4 @@
+#Method 1: stack helper synchronize with stack data
 class MinStack(object):
 
     def __init__(self):
@@ -18,7 +19,7 @@ class MinStack(object):
         if len(self.helper)==0 or x<=self.helper[-1]:
             self.helper.append(x)
         else:
-            self.helper.append(self.helper[-1])
+            self.helper.append(self.helper[-1])# To keep the sychronization,we must append the duplicate element  
 
 
     def pop(self):
@@ -29,6 +30,63 @@ class MinStack(object):
             self.helper.pop()
             return self.helper.pop()
         
+
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if self.data:
+            return self.data[-1]
+
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if self.helper:
+            return self.helper[-1]
+
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+#Method 2: stack helper do not synchronize with stack data
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.data = []
+        self.helper = []
+
+
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.data.append(x)
+        if len(self.helper)==0 or x<=self.helper[-1]:
+            self.helper.append(x)
+        
+
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        top = self.data.pop()
+        if self.helper  and top==self.helper[-1]:# Poping the element Only the data stack's element equals to helper stack's element
+            self.helper.pop()
+        return top 
 
 
     def top(self):
